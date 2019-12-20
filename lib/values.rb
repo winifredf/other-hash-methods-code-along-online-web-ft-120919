6d7e3145cccb9f6aa9d5b8f15535c1586263fcd2
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 06:06:30 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,7 +9,12 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  
+  groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
   groceries.keys
   #code your solution here!
 end
